7de16fa4945cb68959b16b70f7a10a529e3861cd
kauhiant <kauhia3440@gmail.com>
2017-09-10 21:14:21 +0800

    [0.91] fixed bug in level 7 : AI no warrior sometimes

--- LittleWarGame/PlayBoard.cs
+++ LittleWarGame/PlayBoard.cs
@@ -176,6 +176,7 @@ namespace LittleWarGame
                     isCreate = addRescue();
                     break;
                 default:
+                    isCreate = true;
                     break;
             }
 
