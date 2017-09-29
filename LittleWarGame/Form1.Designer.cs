a0be473ffb3057743f0d0114b1a3924e109a9489
kauhiant <kauhia3440@gmail.com>
2017-09-29 15:57:06 +0800

    maximizebox = false

--- LittleWarGame/Form1.Designer.cs
+++ LittleWarGame/Form1.Designer.cs
@@ -240,6 +240,7 @@
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Little War Game";
